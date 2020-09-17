65f60e168cbd8ef2416828394009057615706cd3
rose-line <patrice.gahide@gmail.com>
2020-09-17 16:43:54 +0200

    Améliore l'encapsulation de la classe Compte

--- src/main/java/fr/pgah/slam5/Banque.java
+++ src/main/java/fr/pgah/slam5/Banque.java
@@ -5,9 +5,7 @@ import java.util.Set;
 
 public class Banque {
   private HashMap<Integer, Compte> comptes = new HashMap<>();
-  private double taux = 0.01;
   private int numDernierCompte = 0;
-
 
   public int creerCompte(boolean estEtranger) {
     int numCompte = numDernierCompte++;
@@ -22,16 +20,12 @@ public class Banque {
 
   public void crediter(int numCompte, int montant) {
     Compte compte = comptes.get(numCompte);
-    int solde = compte.getSolde();
-    int nouveauSolde = solde + montant;
-    compte.setSolde(nouveauSolde);
+    compte.crediter(montant);
   }
 
   public void appliquerInterets() {
     for (Compte compte : comptes.values()) {
-      int solde = compte.getSolde();
-      int nouveauSolde = (int) (solde * (1 + taux));
-      compte.setSolde(nouveauSolde);
+      compte.appliquerInterets();
     }
   }
 
--- src/main/java/fr/pgah/slam5/Compte.java
+++ src/main/java/fr/pgah/slam5/Compte.java
@@ -3,6 +3,7 @@ package fr.pgah.slam5;
 public class Compte {
   private int solde;
   private boolean estEtranger;
+  private double taux = 0.01;
 
   public Compte(boolean estEtranger) {
     this.estEtranger = estEtranger;
@@ -12,11 +13,15 @@ public class Compte {
     return solde;
   }
 
-  public void setSolde(int montant) {
-    solde = montant;
-  }
-
   public boolean getEstEtranger() {
     return estEtranger;
+  }
+
+  public void crediter(int montant) {
+    solde = solde + montant;
+  }
+
+  public void appliquerInterets() {
+    solde = (int) (solde * (1 + taux));
   }
 }
